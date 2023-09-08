912aecd9f33628a2012c7d1877867992e10f8de5
nakanaka773 <hirayama773@gmail.com>
2023-09-08 14:44:19 +0900

    prism.jsでシンタックスハイライトを足しました

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -3,18 +3,23 @@ import Writer from "@/components/Writer";
 import { client } from "@/libs/client";
 import sanitizeHtml from 'sanitize-html';
 import styles from '../../styles/id.module.css';
-
-
+import Prism from "prismjs";
+import "prismjs/components/prism-javascript"; 
+import { useEffect } from "react";
+import 'prismjs/themes/prism.css';
 
 export const getStaticProps = async (context) => {
     const id = context.params.id;
     const data = await client.get({endpoint: "blognana", contentId: id})
+    
+
 
     return {
         props: {
             blog: data,
         },
     };
+    
 
 };
 
@@ -28,19 +33,13 @@ export const getStaticPaths = async () => {
     };
 };
 
+
+
 export default function BlogId({ blog }) {
-    const cleanHtml = sanitizeHtml(blog.body, {
-        allowedTags: ['b', 'i', 'em', 'strong', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a', 'ul', 'li', 'ol', 'br', 'span', 'img', 'code'],
-        allowedAttributes: {
-            'a': [ 'href' ],
-            'img': [ 'src' ]
-        },
-        selfClosing: ['img', 'br'],
-        allowedSchemes: ['http', 'https', 'ftp', 'mailto'],
-        transformTags: {
-            'a': sanitizeHtml.simpleTransform('a', {rel: 'nofollow'})
-        }
-    });
+
+    useEffect(() => {
+        Prism.highlightAll();
+      }, []);
 
     return(
         <main>
@@ -50,10 +49,11 @@ export default function BlogId({ blog }) {
                 <h1 className={styles.title}>{blog.title}</h1>
                 <p className={styles.publishdate}>{new Date(blog.publishedAt).toISOString().split('T')[0]}</p>
                 <img className={styles.topImg} src={blog.topImg.url} alt="" />
-                <div dangerouslySetInnerHTML={{__html: cleanHtml}}></div>
+                <div dangerouslySetInnerHTML={{__html: blog.body}}></div>
             </div>
             <Writer className={styles.writer} />
             </div>
         </main>
+        
     );
 }
